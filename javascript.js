7ffaf5f4914c4311c321bb4a1a0bfc1a25bd0174
demin <dtan189@aucklanduni.ac.nz>
2015-06-03 00:52:17 +0000

    add five files to local git

---
+++ javascript.js
@@ -0,0 +1,22 @@
+	function bulbControl()
+	{	
+		if (document.getElementById("logo1").getAttribute('src') === 'images/turnon.png')
+		{
+			document.getElementById("logo1").src="images/turnoff.png"
+		}
+		else
+		{
+			document.getElementById("logo1").src="images/turnon.png";
+		}
+	}
+		function bulbControl()
+	{	
+		if (document.getElementById("logo2").getAttribute('src') === 'images/turnon.png')
+		{
+			document.getElementById("logo2").src="images/turnoff.png"
+		}
+		else
+		{
+			document.getElementById("logo2").src="images/turnon.png";
+		}
+	}
